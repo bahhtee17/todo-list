b665bab5979ac085b9cb3559fdaf1e85d5fbe5ac
Baxtiyor <baxtiyorshukurjanov36@gmail.com>
2021-08-05 00:00:32 +0500

    a

--- src/components/Item.vue
+++ src/components/Item.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="container">
-    <input type="checkbox"/>
+    <button class="delete">X</button>
     <h4>{{item}}</h4>
-    <button>X</button>
+    <input class="checkbox" type="checkbox"/>
   </div>
 </template>
 
@@ -14,5 +14,37 @@ export default {
 </script>
 
 <style>
+  .container{
+    display: inline-table;
+    border: 0px;
+    border-radius: 10px;
+    width: 30%;
+    background-color:yellow;
+    margin-bottom: 10px;
+    margin-left: 10px;
+    box-shadow: 2px 2px 5px;
 
+  }
+
+
+  .checkbox:hover{
+    width: 30px;
+    height: 30px;
+    border: none;
+    border-radius: 100px;
+  }
+
+  button:hover{
+    background-color: rgb(250, 131, 131);
+    width: 30px;
+    height: 30px;
+  }
+
+  .delete{
+   width: 10%;
+    border: none;
+    border-radius: 10px;
+    background: rgb(255, 80, 80);
+    margin-left: 88%;
+  }
 </style>
